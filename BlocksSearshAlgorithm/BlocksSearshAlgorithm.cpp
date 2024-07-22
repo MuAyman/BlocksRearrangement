c730658b31565522b0bd89ec4783010947010e5c
MuAyman <s-mo_ayman@zewailcity.edu.eg>
2024-07-22 20:37:01 +0300

    trial for A*

--- BlocksSearshAlgorithm/BlocksSearshAlgorithm.cpp
+++ BlocksSearshAlgorithm/BlocksSearshAlgorithm.cpp
@@ -24,18 +24,11 @@ public: // not worth it in the context of a test task
 	State state;
 	Moves move;
 	int fixedCount; // how close you are to the goal
-	// int heuristic;   // heuristic value to guide the search
 
 	StateSpace() : state({}), move({}), fixedCount(0) {}
 	StateSpace(State s) : state(s), move({}), fixedCount(0) {}
 	StateSpace(State s, Moves m) : state(s), move(m), fixedCount(0) {}
 	StateSpace(State s, Moves m, int f) : state(s), move(m), fixedCount(f) {}
-
-	// for the priority queue comparison
-	bool operator<(const StateSpace &other) const
-	{
-		return (fixedCount) < (other.fixedCount);
-	}
 };
 
 // function to compress state vectors to strings (with sorted stacks meaning
@@ -184,17 +177,16 @@ void countMatches(StateSpace &current, const State &goal)
 // BFS algorithm to find the optimal path to reach the goal state
 Moves BFS_Algorithm(const State &start, const State &goal)
 {
-	// Moves path;
-	priority_queue<StateSpace> BFSq; // priority queue to prioritize the states nearer to the goal
-	unordered_set<string> visited;   // unordered map to enhance the efficacy of the searching in it every iteration
+	queue<StateSpace> BFSq;		 // queue for BFS
+	unordered_set<string> visited; // unordered map to enhance the efficacy of the searching in it every iteration
 
-	BFSq.push(start);
+	StateSpace startState(start);
+	BFSq.push(startState);
 	visited.insert(toString(start)); // store the visited states as a string for ease of access and comparison
 
 	while (!BFSq.empty())
 	{
-
-		StateSpace current = BFSq.top();
+		StateSpace current = BFSq.front();
 		BFSq.pop();
 		if (IsGoal(current.state, goal))
 			return current.move;
@@ -220,83 +212,29 @@ Moves BFS_Algorithm(const State &start, const State &goal)
 void compare(State in, State goal, State sol, int n)
 {
 	vector<pair<string, string>> path = BFS_Algorithm(in, goal);
-	cout << "\n\t" << n << ". my sol moves: " << path.size() << "\n\t      Test moves: " << sol.size() << "\n";
+	cout << "\n\t" << n << ". my sol moves: " << path.size() << "\n\t";
+	for (int i = 0; i < path.size(); ++i)
+		cout << "\t" << i + 1 << ". Move " << path[i].first << " to " << path[i].second << "\n\t";
+
+	cout << "\n\toptimal sol moves: " << sol.size() << "\n\t";
+	for (int i = 0; i < sol.size(); ++i)
+		cout << "\t" << i + 1 << ". Move " << sol[i].front().letter << " to " << sol[i].back().letter << "\n\t";
 }
 
 int main()
 {
+	cout << "all the sol moves are 1-indexed to match the format of the problem statement\n";
 
-	// 	State inputz{'B', '-'},
-	// 	{'A', 'E'},
-	// 	{'B', 'A'},
-	// 	{'C', 'B'}
-	// };
-	// compare(input11, goal11, sol11);
+	// initialize the start and goal states
+	State start = {{Block('A'), Block('B')}, {Block('C'), Block('D')}, {}};
+	State goal = {{Block('A')}, {Block('B')}, {Block('C'), Block('D')}};
+	State sol = {{Block('A'), Block('B')}, {Block('C')}, {Block('D')}};
 
-	State input1 = {{'A', 'B', 'C'}, {'D', 'E'}};
-	State goal1 = {{'A', 'C'}, {'D', 'E', 'B'}};
-	State sol1 = {{'C', '-'}, {'B', 'E'}, {'C', 'A'}};
-	compare(input1, goal1, sol1, 1);
+	// compare my solution with the optimal solution
+	compare(start, goal, sol, 1);
 
-	State input2 = {{'D', 'H', 'B', 'E'}, {'G', 'A'}, {'I', 'C', 'J', 'F'}};
-	State goal2 = {{'D', 'C'}, {'G', 'E'}, {'F', 'B', 'A', 'H'}, {'J', 'I'}};
-	State sol2 = {{'A', '-'}, {'E', 'G'}, {'F', '-'}, {'B', 'F'}, {'A', 'B'}, {'H', 'A'}, {'J', '-'}, {'C', 'D'}, {'I', 'J'}};
-	compare(input2, goal2, sol2, 2);
+	// other test cases (1 per line)
+	compare({{Block('A'), Block('B')}, {Block('C')}, {Block('D')}}, {{Block('A')}, {Block('B')}, {Block('C'), Block('D')}}, {{Block('A'), Block('B')}, {Block('C'), Block('D')}}, 2);
 
-	State input3 = {{'E', 'N', 'H', 'O'}, {'L', 'F', 'K', 'B'}, {'M', 'C'}, {'A', 'I', 'J'}, {'G', 'D'}};
-	State goal3 = {{'O', 'A', 'K', 'D', 'J', 'N'}, {'I'}, {'F', 'H', 'C'}, {'E', 'L', 'M'}, {'G', 'B'}};
-	State sol3 = {{'J', '-'}, {'I', '-'}, {'O', '-'}, {'A', 'O'}, {'B', '-'}, {'K', 'A'}, {'D', 'K'}, {'B', 'G'}, {'J', 'D'}, {'H', '-'}, {'N', 'J'}, {'F', '-'}, {'L', 'E'}, {'H', 'F'}, {'C', 'H'}, {'M', 'L'}};
-	compare(input3, goal3, sol3, 3);
-
-	State input4 = {{'D', 'I'}, {'J', 'K', 'E', 'A', 'H', 'C', 'G'}, {'F', 'B'}};
-	State goal4 = {{'A', 'C', 'B'}, {'G'}, {'K'}, {'F', 'H', 'I', 'E', 'J', 'D'}};
-	State sol4 = {{'G', '-'}, {'I', '-'}, {'C', 'D'}, {'B', 'C'}, {'H', 'F'}, {'I', 'H'}, {'A', '-'}, {'E', 'I'}, {'K', '-'}, {'J', 'E'}, {'B', '-'}, {'C', 'A'}, {'D', 'J'}, {'B', 'C'}};
-	compare(input4, goal4, sol4, 4);
-
-	State input5 = {{'D', 'G', 'C', 'P', 'J', 'O', 'I', 'H'}, {'A', 'Q', 'M', 'K'}, {'L', 'F'}, {'B', 'N', 'E'}};
-	State goal5 = {{'H', 'B'}, {'E', 'P', 'D', 'K'}, {'O', 'Q', 'G'}, {'N', 'M', 'C'}, {'F', 'I', 'J'}, {'L', 'A'}};
-	State sol5 = {{'H', '-'}, {'F', '-'}, {'I', 'F'}, {'O', '-'}, {'J', 'I'}, {'E', '-'}, {'P', 'E'}, {'N', '-'}, {'B', 'H'}, {'K', '-'}, {'M', 'N'}, {'C', 'M'}, {'G', 'Q'}, {'D', 'P'}, {'K', 'D'}, {'G', '-'}, {'Q', 'O'}, {'A', 'L'}, {'G', 'Q'}};
-	compare(input5, goal5, sol5, 5);
-
-	State input6 = {{'F', 'B', 'C'}, {'H', 'I'}, {'M', 'E', 'J'}, {'L', 'D', 'A', 'G', 'K'}};
-	State goal6 = {{'D', 'H', 'J', 'E', 'C'}, {'L'}, {'G', 'B', 'M'}, {'I', 'A', 'F'}, {'K'}};
-	State sol6 = {{'K', '-'}, {'C', '-'}, {'G', '-'}, {'B', 'G'}, {'A', '-'}, {'D', '-'}, {'I', '-'}, {'H', 'D'}, {'J', 'H'}, {'E', 'J'}, {'M', 'B'}, {'C', 'E'}, {'A', 'I'}, {'F', 'A'}};
-	compare(input6, goal6, sol6, 6);
-
-	State input7 = {{'J', 'B', 'A', 'E', 'O', 'N'}, {'I', 'G', 'K', 'P', 'Q', 'D', 'L', 'H'}, {'F'}, {'M', 'C'}};
-	State goal7 = {{'O', 'E', 'H', 'L'}, {'A'}, {'M', 'I', 'F'}, {'B', 'K', 'G', 'N'}, {'P'}, {'J', 'Q', 'D'}, {'C'}};
-	State sol7 = {{'C', '-'}, {'N', '-'}, {'O', '-'}, {'E', 'O'}, {'A', '-'}, {'H', 'E'}, {'L', 'H'}, {'B', '-'}, {'D', '-'}, {'Q', 'J'}, {'D', 'Q'}, {'P', '-'}, {'K', 'B'}, {'G', 'K'}, {'N', 'G'}, {'I', 'M'}, {'F', 'I'}};
-	compare(input7, goal7, sol7, 7);
-
-	State input8 = {{'O', 'B', 'D'}, {'F', 'Q', 'I', 'P', 'A'}, {'C', 'E', 'M', 'G'}, {'R', 'N', 'L'}, {'H', 'J'}, {'K'}};
-	State goal8 = {{'M', 'G', 'I', 'O'}, {'J', 'L', 'P'}, {'H', 'Q', 'D', 'E', 'C', 'B'}, {'N', 'K'}, {'R', 'A', 'F'}};
-	State sol8 = {{'J', '-'}, {'L', 'J'}, {'A', '-'}, {'P', 'L'}, {'I', '-'}, {'Q', 'H'}, {'D', 'Q'}, {'N', '-'}, {'K', 'N'}, {'A', 'R'}, {'F', 'A'}, {'G', '-'}, {'M', '-'}, {'E', 'D'}, {'C', 'E'}, {'B', 'C'}, {'G', 'M'}, {'I', 'G'}, {'O', 'I'}};
-	compare(input8, goal8, sol8, 8);
-
-	State input9 = {{'C', 'B', 'F', 'O', 'D'}, {'I', 'L', 'H', 'G', 'P', 'K', 'N'}, {'J', 'E'}, {'A', 'M'}};
-	State goal9 = {{'A', 'K'}, {'C', 'G', 'F', 'O'}, {'D', 'H', 'J'}, {'B', 'I'}, {'M', 'P'}, {'E', 'L'}, {'N'}};
-	State sol9 = {{'N', '-'}, {'M', '-'}, {'K', 'A'}, {'P', 'M'}, {'D', '-'}, {'G', '-'}, {'H', 'D'}, {'E', '-'}, {'L', 'E'}, {'J', 'H'}, {'O', '-'}, {'F', '-'}, {'O', 'F'}, {'B', '-'}, {'I', 'B'}, {'G', 'C'}, {'O', '-'}, {'F', 'G'}, {'O', 'F'}};
-	compare(input9, goal9, sol9, 9);
-
-	State input10 = {{'J', 'H', 'I', 'D', 'O', 'P', 'B', 'F', 'G', 'C', 'E', 'K', 'A'}, {'Q', 'L'}, {'N', 'M'}};
-	State goal10 = {{'P', 'N', 'H', 'C'}, {'O', 'K', 'Q', 'J', 'D', 'B', 'I'}, {'M', 'E', 'A'}, {'G', 'L'}, {'F'}};
-	State sol10 = {{'A', '-'}, {'K', '-'}, {'M', '-'}, {'E', 'M'}, {'A', 'E'}, {'C', '-'}, {'G', '-'}, {'L', 'G'}, {'F', '-'}, {'B', '-'}, {'P', '-'}, {'N', 'P'}, {'O', '-'}, {'K', 'O'}, {'Q', 'K'}, {'D', '-'}, {'I', '-'}, {'H', 'N'}, {'C', 'H'}, {'J', 'Q'}, {'D', 'J'}, {'B', 'D'}, {'I', 'B'}};
-	compare(input10, goal10, sol10, 10);
-
-	State input11 = {{'A', 'B', 'C'}, {'D', 'E'}};
-	State goal11 = {{'D', 'E', 'A', 'B', 'C'}};
-	State sol11 = {{'C', '-'}, {'B', '-'}, {'A', 'E'}, {'B', 'A'}, {'C', 'B'}};
-	compare(input11, goal11, sol11, 11);
-
-	State input12 = {{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}};
-	State goal12 = {{'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'}};
-	State sol12 = {{'I', '-'}, {'H', 'I'}, {'G', 'H'}, {'F', 'G'}, {'E', 'F'}, {'D', 'E'}, {'C', 'D'}, {'B', 'C'}, {'A', 'B'}};
-	compare(input12, goal12, sol12, 12);
-
-	State input13 = {{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}};
-	State goal13 = {{'F', 'D', 'C', 'I', 'G', 'A'}, {'B', 'E', 'H'}};
-	State sol13 = {{'F', '-'}, {'E', '-'}, {'D', 'F'}, {'C', 'D'}, {'I', 'C'}, {'B', '-'}, {'E', 'B'}, {'H', '-'}, {'H', 'E'}, {'G', 'I'}, {'A', 'G'}};
-	compare(input13, goal13, sol13, 13);
-	cout << endl;
 	return 0;
 }
